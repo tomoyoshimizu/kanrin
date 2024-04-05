59911c2e9725dce278e0bbd354a95748e6c3b218
tomoyoshimizu <154865008+tomoyoshimizu@users.noreply.github.com>
2024-04-05 14:59:07 +0000

    [Add] controllers and views

---
+++ app/controllers/public/projects_controller.rb
@@ -0,0 +1,25 @@
+class Public::ProjectsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def bookmarks
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
